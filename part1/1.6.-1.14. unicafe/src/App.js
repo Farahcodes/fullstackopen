c6ec871325be299b0e1b3ae2f4863a609ff738e6
farah <farah.fournat@gmail.com>
2023-03-15 13:39:58 +0100

    give feedback component

--- part1/1.6.-1.14. unicafe/src/App.js
+++ part1/1.6.-1.14. unicafe/src/App.js
@@ -1,12 +1,35 @@
 import { useState } from "react";
 
+const good = "good";
+const neutral = "neutral";
+const bad = "bad";
+
+const Button = (props) => {
+  return <button>{props.text}</button>;
+};
+
+const GiveFeedback = (props) => {
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button text={good} />
+      <Button text={neutral} />
+      <Button text={bad} />
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <GiveFeedback />
+    </div>
+  );
 };
 
 export default App;
