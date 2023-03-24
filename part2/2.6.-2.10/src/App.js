d7ba7a052c8200805a47f88d93ae8cfbf222a93c
farah <farah.fournat@gmail.com>
2023-03-24 15:35:24 +0100

    added Person component

--- part2/2.6.-2.10/src/App.js
+++ part2/2.6.-2.10/src/App.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
 
+// components
+import Person from "./components/Person";
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <Person key={person.name} name={person.name} />
+        ))}
+      </ul>
+      <div>debug: {newName}</div>
     </div>
   );
 };
---
+++ part2/2.6.-2.10/src/components/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Person = (props) => {
+  return (
+    <li>
+      <span>{props.name}</span>
+    </li>
+  );
+};
+
+export default Person;
