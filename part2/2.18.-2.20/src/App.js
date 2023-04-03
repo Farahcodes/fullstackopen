70157861dcc10ba99f24d0ea7c51a20252c89c6b
farah <farah.fournat@gmail.com>
2023-04-03 08:53:51 +0200

    initial state

--- part2/2.18.-2.20/src/App.js
+++ part2/2.18.-2.20/src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import countryService from "./services/countries";
 
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <div>
       <h2>Countries</h2>
