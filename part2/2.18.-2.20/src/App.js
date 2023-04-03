8942edddf9f7678d1b9b4b0b085cec9277aa2919
farah <farah.fournat@gmail.com>
2023-04-03 08:37:13 +0200

    init first commit

---
+++ part2/2.18.-2.20/src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Countries</h2>
+    </div>
+  );
+};
+
+export default App;
