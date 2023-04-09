844285368a39d0b0a11b38744aeac6851b0ed180
farahcodes <farah.fournat@gmail.com>
2023-04-09 17:13:12 +0200

    step 1 completed

--- part3/3.1.-3.6./index.js
+++ part3/3.1.-3.6./index.js
@@ -25,3 +25,16 @@ let persons = [
     number: '39-23-6423122',
   },
 ];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
