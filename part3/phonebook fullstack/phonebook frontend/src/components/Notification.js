ac967f0eeea2a77a50d1bbc31c29879eebbde118
farahcodes <farah.fournat@gmail.com>
2023-04-13 11:05:28 +0200

    phonebook fullstack structure file

---
+++ part3/phonebook fullstack/phonebook frontend/src/components/Notification.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null;
+  }
+
+  const notificationClass = type === "error" ? "error" : "success";
+
+  return <div className={notificationClass}>{message}</div>;
+};
+
+export default Notification;
