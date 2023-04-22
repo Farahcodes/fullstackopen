5672a6d04d75c87a05c8b72af34b93ce10109e80
farahcodes <farah.fournat@gmail.com>
2023-04-22 12:40:58 +0200

    adding a new person

---
+++ part3/phonebook fullstack/phonebook backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://ffournat:${password}@cluster0.tqeeu2x.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+  id: 'c9d37ec9-6c5f-47c8-a625-311d46723b97',
+  name: 'Nora',
+  number: '03030303',
+});
+
+person.save().then((result) => {
+  console.log('person saved!');
+  mongoose.connection.close();
+});
+
+// Note.find({}).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
