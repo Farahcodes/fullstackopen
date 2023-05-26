03cb98097d33a3f7ac817fdb9f6fa1a63bfccd97
farahcodes <farah.fournat@gmail.com>
2023-05-26 20:53:31 +0200

    updated code using dotenv

--- part4-blogList-backend/index.js
+++ part4-blogList-backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -12,8 +13,18 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log('error connecting to MongoDB:', err.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -32,7 +43,7 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
