c0a9683fa5e8238a077b3ea4e27838b2cc0c263f
farahcodes <farah.fournat@gmail.com>
2023-09-30 21:44:44 +0200

    response status 400 if missing title

--- part4-blogList-backend/tests/blog_api.test.js
+++ part4-blogList-backend/tests/blog_api.test.js
@@ -77,6 +77,16 @@ test('if likes property is missing, it defaults to zero', async () => {
     url: 'http://anothernewblog.com/',
   };
 
+  test('if title is missing, response status is 400', async () => {
+    const newBlog = {
+      author: 'Missing Title Author',
+      url: 'http://missingtitle.com/',
+      likes: 5,
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
+
   const response = await api
     .post('/api/blogs')
     .send(newBlog)
