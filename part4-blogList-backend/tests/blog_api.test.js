32de4385abe09f5d30f28059ee3c7f14b363418c
farahcodes <farah.fournat@gmail.com>
2023-11-01 20:40:35 +0100

    testing DELETE functionality

--- part4-blogList-backend/tests/blog_api.test.js
+++ part4-blogList-backend/tests/blog_api.test.js
@@ -105,3 +105,18 @@ test('if url is missing, response status is 400', async () => {
 
   await api.post('/api/blogs').send(newBlog).expect(400);
 }, 10000);
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await Blog.find({});
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await Blog.find({});
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1);
+
+    const titles = blogsAtEnd.map((blog) => blog.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+});
