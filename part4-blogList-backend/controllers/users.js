fb15e65106379cc9ed1bece72515da889deece36
farahcodes <farah.fournat@gmail.com>
2023-11-26 20:10:06 +0100

    additional validation to user creation route

--- part4-blogList-backend/controllers/users.js
+++ part4-blogList-backend/controllers/users.js
@@ -6,6 +6,26 @@ const User = require('../models/user');
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (!username || !password) {
+    return response
+      .status(400)
+      .json({ error: 'Username and password are required' });
+  }
+
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error:
+        'Username and password must be at least 3 characters long',
+    });
+  }
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response
+      .status(400)
+      .json({ error: 'Username must be unique' });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
@@ -15,9 +35,12 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   });
 
-  const savedUser = await user.save();
-
-  response.status(201).json(savedUser);
+  try {
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
 });
 
 usersRouter.get('/', async (request, response) => {
