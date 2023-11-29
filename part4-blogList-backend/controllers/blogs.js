148718da8f5f54304dba7d08a7d6108c22b92f42
farahcodes <farah.fournat@gmail.com>
2023-11-29 22:21:18 +0100

    populate the user field

--- part4-blogList-backend/controllers/blogs.js
+++ part4-blogList-backend/controllers/blogs.js
@@ -4,10 +4,12 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
   });
+  response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
