e3b0b658b1bacb8494a0011791d3b4fa700645b9
farahcodes <farah.fournat@gmail.com>
2024-02-19 21:36:19 +0100

    blog-list-frontend: implement login functionality as per ex. 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,30 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import BlogList from './components/BlogList';
+import Login from './components/Login';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    const setInitialBlogs = async () => {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
+    };
+
+    setInitialBlogs();
+  }, []);
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+    <>
+      {!user ? (
+        <Login setUser={setUser} />
+      ) : (
+        <BlogList blogs={blogs} user={user} />
       )}
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
