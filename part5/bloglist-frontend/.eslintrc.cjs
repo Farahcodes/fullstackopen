335802cf8db8d2409a38b61e0b1b52c014504f07
farahcodes <farah.fournat@gmail.com>
2024-03-15 12:12:30 +0100

    cypress config

--- part5/bloglist-frontend/.eslintrc.cjs
+++ part5/bloglist-frontend/.eslintrc.cjs
@@ -3,17 +3,19 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
+    'cypress/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:cypress/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh, cypress'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
