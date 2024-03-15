c1c9b347d3b1854fbef4ffb79330c68ff19073d1
farahcodes <farah.fournat@gmail.com>
2024-03-15 14:58:33 +0100

    5.17: cypress test ok

---
+++ part5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('Log in to Application');
+    cy.get('#login-form').should('be.visible');
+  });
+});
