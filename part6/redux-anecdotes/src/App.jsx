5f1a0b096893e9f4d3ee6c904f78d832fe1a1a09
farahcodes <farah.fournat@gmail.com>
2024-03-30 14:41:36 +0100

    6.8 ok AnecdoteList

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,35 +1,13 @@
-// @ts-nocheck
 /* eslint-disable no-unused-vars */
-import React, { useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { voteForAnecdote } from './reducers/anecdoteReducer';
-// components/AnecdoteForm.js
+
+import React from 'react';
+import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  );
-  const dispatch = useDispatch();
-  const inputRef = useRef();
-
-  // function to dispatch the vote action
-  const vote = (id) => {
-    dispatch(voteForAnecdote(id));
-  };
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,33 @@
+// @ts-nocheck
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { voteForAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) =>
+    [...state].sort((a, b) => b.votes - a.votes)
+  );
+
+  const vote = (id) => {
+    dispatch(voteForAnecdote(id));
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
