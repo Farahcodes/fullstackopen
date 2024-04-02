86f3ebc47bca219c9b4b5eb09dd1575bccb14ba7
farahcodes <farah.fournat@gmail.com>
2024-04-02 12:39:51 +0200

    fetch the anecdotes from the backend 6.14 ok

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+// components
 import AnecdoteList from './components/AnecdoteList';
 import AnecdoteForm from './components/AnecdoteForm';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
+// services
+import anecdoteService from './services/anecdotes';
+// reducers
+import { setAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => {
+      dispatch(setAnecdotes(anecdotes));
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <Notification />
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,12 +1,23 @@
+/* eslint-disable no-unused-vars */
 // @ts-nocheck
 import ReactDOM from 'react-dom/client';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+
+// components
 import App from './App';
+// reducers
 import rootReducer from './reducers/index';
+import { setAnecdotes } from './reducers/anecdoteReducer';
+// services
+import anecdoteService from './services/anecdotes';
 
 const store = configureStore({
   reducer: rootReducer,
+});
+
+anecdoteService.getAll().then((anecdotes) => {
+  store.dispatch(setAnecdotes(anecdotes));
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
