3599cd1052537a2a4e04ef2412bb64df45c51b8c
farahcodes <farah.fournat@gmail.com>
2024-04-04 17:58:04 +0200

    clean code

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,24 +1,12 @@
 /* eslint-disable no-unused-vars */
 // @ts-nocheck
 import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
+// store
+import store from './store/store';
 // components
 import App from './App';
-// reducers
-import rootReducer from './reducers/index';
-import { setAnecdotes } from './reducers/anecdoteReducer';
-// services
-import anecdoteService from './services/anecdotes';
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-anecdoteService.getAll().then((anecdotes) => {
-  store.dispatch(setAnecdotes(anecdotes));
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
