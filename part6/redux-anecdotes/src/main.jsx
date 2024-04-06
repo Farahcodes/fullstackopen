385612495d4b11325f046001149aae21ae39f27c
farahcodes <farah.fournat@gmail.com>
2024-04-06 09:10:12 +0200

    clean code

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,22 +2,14 @@
 // @ts-nocheck
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
 
 // store
 import store from './store/store';
 // components
 import App from './App';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </Provider>
 );
