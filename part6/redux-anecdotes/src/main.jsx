34547d75ebdcbce67afbe53b4ebfce0945b973ec
farahcodes <farah.fournat@gmail.com>
2024-04-06 08:53:10 +0200

    installed react-query

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,14 +2,22 @@
 // @ts-nocheck
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 // store
 import store from './store/store';
 // components
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
