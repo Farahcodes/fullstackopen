54e4e030f65acb62aaae30b8e17c953e7390fba9
farahcodes <farah.fournat@gmail.com>
2024-04-15 14:36:38 +0200

    set up notification reducer

---
+++ part7/bloglist-frontend- extended/src/reducers/notificationReducer.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = '';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload;
+    },
+    clearNotification() {
+      return '';
+    },
+  },
+});
+
+export const { setNotification, clearNotification } =
+  notificationSlice.actions;
+
+// Thunk action for showing a notification for 5 seconds
+export const showNotification = (message) => {
+  return async (dispatch) => {
+    dispatch(setNotification(message));
+    setTimeout(() => {
+      dispatch(clearNotification());
+    }, 5000);
+  };
+};
+
+export default notificationSlice.reducer;
