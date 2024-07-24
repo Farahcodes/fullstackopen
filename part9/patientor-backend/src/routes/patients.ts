b3ef26efb58d6439b9ab9bf15077383104aec1bd
farahcodes <farah.fournat@gmail.com>
2024-07-24 18:39:02 +0200

    Create POST endpoint

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-
+// services
 import patientService from '../services/PatientService';
+// utils
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
@@ -8,10 +10,20 @@ router.get('/', (_req, res) => {
   res.send(patientService.getPublicPatients());
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a patient!');
+
+router.post('/api/patients', (req, res) => {
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'An error occurred';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
-
-
 export default router;
+
